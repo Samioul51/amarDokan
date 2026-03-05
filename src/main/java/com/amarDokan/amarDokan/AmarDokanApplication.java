cfe33ca4cb5a584ba074fae3f8e25c230e183988
Samioul51 <akmsamioulislam51@gmail.com>
2026-03-05 23:47:51 +0600

    First commit

---
+++ src/main/java/com/amarDokan/amarDokan/AmarDokanApplication.java
@@ -0,0 +1,13 @@
+package com.amarDokan.amarDokan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarDokanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarDokanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amarDokan/amarDokan/AmarDokanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarDokan.amarDokan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmarDokanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
