809c131b733609d0222ecc31ed7dcd9259568e01
Samioul51 <akmsamioulislam51@gmail.com>
2026-03-08 01:25:37 +0600

    feat: implemented category service interface and implementation

---
+++ src/main/java/com/amarDokan/amarDokan/service/implementations/CategoryServiceImpl.java
@@ -0,0 +1,64 @@
+package com.amarDokan.amarDokan.service.implementations;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import com.amarDokan.amarDokan.models.Category;
+import com.amarDokan.amarDokan.repository.CategoryRepository;
+import com.amarDokan.amarDokan.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Boolean existCategory(String name) {
+        return categoryRepository.existsByName(name);
+    }
+
+    @Override
+    public Boolean deleteCategory(Long id) {
+        Category category = categoryRepository.findById(id).orElse(null);
+
+        if (!ObjectUtils.isEmpty(category)) {
+            categoryRepository.delete(category);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Category> getAllActiveCategory() {
+        return categoryRepository.findByIsActiveTrue();
+    }
+
+    @Override
+    public Page<Category> getAllCategorPagination(Integer pageNo, Integer pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        return categoryRepository.findAll(pageable);
+    }
+
+}
