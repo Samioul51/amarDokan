c2756fb81c467a9063b5f1b164df7b37fcc34e2a
Samioul51 <akmsamioulislam51@gmail.com>
2026-03-07 20:35:51 +0600

    feat: implemented order address repository for shipping details

---
+++ src/main/java/com/amarDokan/repository/OrderAddressRepository.java
@@ -0,0 +1,10 @@
+package com.amarDokan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.amarDokan.amarDokan.models.OrderAddress;
+
+@Repository
+public interface OrderAddressRepository extends JpaRepository<OrderAddress, Long> {
+    // Basic CRUD operations are inherited from JpaRepository
+}
