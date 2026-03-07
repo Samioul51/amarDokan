afac0dd0df03b716922ab27db880fa5cdacc3a7d
Samioul51 <akmsamioulislam51@gmail.com>
2026-03-07 21:30:35 +0600

    fix: moved repositories to correct package structure

--- src/main/java/com/amarDokan/amarDokan/repository/OrderAddressRepository.java
+++ src/main/java/com/amarDokan/amarDokan/repository/OrderAddressRepository.java
@@ -1,4 +1,4 @@
-package com.amarDokan.repository;
+package com.amarDokan.amarDokan.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
