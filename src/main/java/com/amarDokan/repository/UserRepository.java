fe59d3d5f5b9108e942f126aeda6b25d7db1a1c2
Samioul51 <akmsamioulislam51@gmail.com>
2026-03-07 20:12:17 +0600

    feat: implemented user repository with custom finder methods

---
+++ src/main/java/com/amarDokan/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.amarDokan.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.amarDokan.amarDokan.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByEmail(String email);
+
+    public List<User> findByRole(String role);
+
+    public User findByResetToken(String token);
+
+    public Boolean existsByEmail(String email);
+}
